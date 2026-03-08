7613f7f61062b6fb7eae5b720434c39af7c30318
Mayank Jain <mayankjain@Mayanks-MacBook-Air.local>
2026-03-08 23:42:15 +0530

    final-changes-done

--- withAdMob.js
+++ withAdMob.js
@@ -1,23 +1,38 @@
-const { withAndroidManifest } = require('@expo/config-plugins');
+const { withAndroidManifest, withProjectBuildGradle } = require('@expo/config-plugins');
 
-module.exports = function withAdMob(config) {
+const APP_ID = 'ca-app-pub-8002961623139540~9864421579';
+
+// 1. Inject AdMob App ID into AndroidManifest.xml
+function withAdMobManifest(config) {
   return withAndroidManifest(config, (cfg) => {
-    const manifest = cfg.modResults;
-    const app = manifest.manifest.application[0];
+    const app = cfg.modResults.manifest.application[0];
     if (!app['meta-data']) app['meta-data'] = [];
-
     const META_NAME = 'com.google.android.gms.ads.APPLICATION_ID';
-    const already = app['meta-data'].find(
-      (m) => m.$['android:name'] === META_NAME
-    );
-    if (!already) {
+    if (!app['meta-data'].find((m) => m.$['android:name'] === META_NAME)) {
       app['meta-data'].push({
-        $: {
-          'android:name': META_NAME,
-          'android:value': 'ca-app-pub-8002961623139540~9864421579',
-        },
+        $: { 'android:name': META_NAME, 'android:value': APP_ID },
       });
     }
     return cfg;
   });
+}
+
+// 2. Inject googleMobileAdsJson = null into root build.gradle
+// so react-native-google-mobile-ads build.gradle doesn't throw
+function withAdMobGradle(config) {
+  return withProjectBuildGradle(config, (cfg) => {
+    if (!cfg.modResults.contents.includes('googleMobileAdsJson')) {
+      cfg.modResults.contents = cfg.modResults.contents.replace(
+        /^allprojects\s*\{/m,
+        `ext {\n    googleMobileAdsJson = null\n}\n\nallprojects {`
+      );
+    }
+    return cfg;
+  });
+}
+
+module.exports = function withAdMob(config) {
+  config = withAdMobManifest(config);
+  config = withAdMobGradle(config);
+  return config;
 };
